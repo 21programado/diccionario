bf8c2ac9012d48469ba771270644d7f3b3ac0891
21programado <21programado@gmail.com>
2026-01-19 20:16:26 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,18 +8,18 @@ const ARCHIVOS = [
 ];
 
 self.addEventListener("install", e => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE).then(c => c.addAll(ARCHIVOS))
   );
 });
 
 self.addEventListener("activate", e => {
+  self.clients.claim();
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
-        keys
-          .filter(k => k !== CACHE)
-          .map(k => caches.delete(k))
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
       )
     )
   );
@@ -30,4 +30,3 @@ self.addEventListener("fetch", e => {
     caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
-
